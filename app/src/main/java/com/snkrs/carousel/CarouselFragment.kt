d03b7adea1327f1a9e602bcfd73ecb76b1897079
Charlie Anderson <charlieanderson@Charlies-Macbook-Pro.local>
2021-05-16 17:28:51 -0400

    add carousel package

---
+++ app/src/main/java/com/snkrs/carousel/CarouselFragment.kt
@@ -0,0 +1,12 @@
+package com.snkrs.carousel
+
+import com.snkrs.R
+import com.snkrs.base.BaseFragment
+
+class CarouselFragment() : BaseFragment<CarouselViewModel>(
+		CarouselViewModel::class.java,
+		R.layout.fragment_carousel_layout
+	)
+{
+
+}
---
+++ app/src/main/java/com/snkrs/carousel/CarouselViewModel.kt
@@ -0,0 +1,10 @@
+package com.snkrs.carousel
+
+import com.snkrs.BaseViewModel
+import com.snkrs.MainRepository
+
+class CarouselViewModel(
+	private val repository: MainRepository
+): BaseViewModel() {
+
+}
